5c75fcb6ee1c8d4eb03e3301061f14afc065192b
akashkum <akashkv9589@gmail.com>
2021-03-24 13:24:42 +0530

    AKash

--- GHS.java
+++ GHS.java
@@ -1,11 +1,9 @@
 class GHS{
 	public static void main(String[] args){
 		System.out.println("Starting out the assignment.");
-<<<<<<< HEAD
+
 		System.out.println("Update by Akash");
-=======
-		System.out.println("Print name: AAYUSH");	
->>>>>>> 2b1603570d68b3d249452bd50a3cd89ea819feca
+
 	}
 
 }
