8958637f4184968a83a3c8dca104687963108533
Aayush Kumar Singh <67497932+aayushk97@users.noreply.github.com>
2021-04-08 23:53:25 +0530

    Update Message.java

--- src/Message.java
+++ src/Message.java
@@ -1,34 +1,25 @@
-class MsgType{
-	static final int CONNECT = 0;
-	static final int INITIATE = 1;
-	static final int TEST = 2;
-	static final int REJECT = 3;
-	static final int ACCEPT = 4;
-	static final int REPORT1 = 5;
-	static final int REPORT2 = 6;
-	static final int CHANGEROOT = 7;
-}
 
 class Message {
-	int type;
+	MESSAGETYPE type;
 	int receipent;
 	int sender;  //can be nodeId.
-	public Message(int type, int sender, int receiver){
+	
+	public Message(MESSAGETYPE type, int sender, int receiver){
 		this.type =type;
 		this.sender = sender;
 		this.receipent = receiver;
 	}
 	
-	// public void send(){
-	// 	//Need to acquire lock...
+	public void send(){
+		//Need to acquire lock...
 
-	// }
+	}
 }
 
 class ConnectMessage extends Message{
 	int level;
 	public ConnectMessage(int sender, int receiver, int level){
-		super(MsgType.CONNECT, sender, receiver);
+		super(MESSAGETYPE.CONNECT, sender, receiver);
 
 		this.level = level;
 	}
@@ -40,7 +31,7 @@ class InitiateMessage extends Message{
 	int state;
 
 	public InitiateMessage(int sender, int receiver, int level, int fragmentName, int state){
-		super(MsgType.INITIATE, sender, receiver);
+		super(MESSAGETYPE.INITIATE, sender, receiver);
 		this.level = level;
 		this.fragmentName = fragmentName;
 		this.state = state;
@@ -52,7 +43,7 @@ class TestMessage extends Message{
 	int fragmentName;
 
 	public TestMessage(int sender, int receiver, int level, int fragmentName){
-		super(MsgType.TEST, sender, receiver);
+		super(MESSAGETYPE.TEST, sender, receiver);
 		this.level = level;
 		this.fragmentName = fragmentName;
 	}
