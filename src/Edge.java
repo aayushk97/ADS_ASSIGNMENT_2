a7c39b2cd08fffd71eb6b3d3d59e6d0b81e701d4
Aayush Kumar Singh <67497932+aayushk97@users.noreply.github.com>
2021-04-08 23:57:21 +0530

    Update Edge.java

--- src/Edge.java
+++ src/Edge.java
@@ -1,50 +1,24 @@
-
-class Message {
-	MESSAGETYPE type;
-	int receipent;
-	int sender;  //can be nodeId.
+class Edge{
+	//https://stackoverflow.com/questions/26421239/locking-cells-of-an-array
+		
+	private int start;
+	private int end;
+	private int weight;
+	private EDGESTATUS status;
 	
-	public Message(MESSAGETYPE type, int sender, int receiver){
-		this.type =type;
-		this.sender = sender;
-		this.receipent = receiver;
-	}
 	
-	public void send(){
-		//Need to acquire lock...
 
+	public Edge(int u, int v, int w, EDGESTATUS st){
+
+		weight = w;
+		status = st;
+		start = u;
+		end = v;
 	}
-}
+	public int getWeight(){ return weight; }
+	public EDGESTATUS getStatus(){ return status; }
+	public int getStart(){ return start; }
+	public int getEnd(){ return end; }
 
-class ConnectMessage extends Message{
-	int level;
-	public ConnectMessage(int sender, int receiver, int level){
-		super(MESSAGETYPE.CONNECT, sender, receiver);
-
-		this.level = level;
-	}
-}
-
-class InitiateMessage extends Message{
-	int level;
-	int fragmentName;
-	int state;
-
-	public InitiateMessage(int sender, int receiver, int level, int fragmentName, int state){
-		super(MESSAGETYPE.INITIATE, sender, receiver);
-		this.level = level;
-		this.fragmentName = fragmentName;
-		this.state = state;
-	}
-}
-
-class TestMessage extends Message{
-	int level;
-	int fragmentName;
-
-	public TestMessage(int sender, int receiver, int level, int fragmentName){
-		super(MESSAGETYPE.TEST, sender, receiver);
-		this.level = level;
-		this.fragmentName = fragmentName;
-	}
+	public void setStatus(EDGESTATUS st){ status = st; }
 }
