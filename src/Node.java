55042964527342d250262bd090f58a57711e8c01
akashkum <akashkv9589@gmail.com>
2021-04-10 00:40:52 +0530

    added proceesInitiate method...still need to be completed

--- src/Node.java
+++ src/Node.java
@@ -6,38 +6,51 @@ class Node implements Runnable{
 	private NodeState state;
 	public int fragmentId;
 	public int level;
-	
+	public int numOfNeighbors;
 	private int[][] neighbors;
-
 	private HashMap<Integer, Integer> neighborsIndex;  //Because index in neighbor array and nodeId are not same
 														// so to get the index of neighbour for a NodeId
 	private Vector<Integer> children;
 	private int parent;	
 	private int rec;
 
+	private int flag;
+	private int waitingCount;
 	private Queue<Message> messageList;
+	private Vector<Message> waitingMessage;
+
+	private int bestNode;
+	private int bestWt;
+	private int testNode;
 
 	public Node(int nodeId, List<Integer> listOfNeighbors, Queue<Message> messageList){
 
 
-		state = NodeState.SLEEP;
-		fragmentId = nodeId; 			// can we use nodeId as fragement id? 
-										//Edit 1: When it is initialized the value of fragment id will be node id. Edit 2: Ok
-		level = Integer.MAX_VALUE; 		// to say it's currently in sleep mode itself. 
+		this.state = NodeState.SLEEP;
+		this.fragmentId = nodeId; 			
+										
+		this.level = Integer.MAX_VALUE; 		// to say it's currently in sleep mode itself. 
 		this.nodeId = nodeId;
-		//int len = listOfNeighbors.length;
-		neighborsIndex = new HashMap<>();	
-		neighbors = new int[listOfNeighbors.size()][5];  // let first column be for: nodeId of nighbor, second: weight, third: status
-		prepareNeighborList(listOfNeighbors);		// fourth: whether its child(0) or parent(1) or neither -1.
-		
-		System.out.println("Printing from node: " + nodeId);
+		this.numOfNeighbors = listOfNeighbors.size();
 
-		for(int i =0 ; i < listOfNeighbors.size(); i++){
-			System.out.println(neighbors[i][0] + " " + neighbors[i][1] + " " + neighbors[i][2]);
-		}
+		this.neighborsIndex = new HashMap<>();	
+		this.neighbors = new int[listOfNeighbors.size()][5];  // let first column be for: nodeId of nighbor, second: weight, third: status
+		prepareNeighborList(listOfNeighbors);		     // fourth: whether its child(0) or parent(1) or neither -1.
+		
+		this.children = new Vector<>();
+		// System.out.println("Printing from node: " + nodeId);
+
+		// for(int i =0 ; i < listOfNeighbors.size(); i++){
+		// 	System.out.println(neighbors[i][0] + " " + neighbors[i][1] + " " + neighbors[i][2]);
+		// }
+
+		this.flag = 0;
+		this.waitingCount = 0;
 		
 		this.messageList = messageList;
+		this.waitingMessage = new Vector<Message>();
 		
+
 	}
 
 	public void run() {
@@ -61,21 +74,23 @@ class Node implements Runnable{
 		
 			System.out.println(nodeId + " Came Here??");
 			Message msg = getMessage();
-			// if(msg!= null){
-			// 	if(msg.type == MessageType.CONNECT){
-			// 		System.out.println(nodeId + " proceesing Connect Message");
-			// 		int flag = processConnectMsg(msg);
-			// 		if(flag == 0 ){
+			if(msg!= null){
+				if(msg.type == MessageType.CONNECT){
+					System.out.println(nodeId + " proceesing Connect Message");
+					int flag = processConnectMsg(msg);
+					if(flag == 0 ){
 
-			// 		}
-			// 	}else if (msg.type == MessageType.INITIATE){
+					}
+				}else if (msg.type == MessageType.INITIATE){
+					System.out.println("Process Initiate Message...");
+					processInitiateMsg(msg);
 
-			// 	}else if(msg.type == MessageType.TEST){
+				}else if(msg.type == MessageType.TEST){
 
-			// 	}else if(msg.type == MessageType.REJECT){
+				}else if(msg.type == MessageType.REJECT){
 					
-			// 	}
-			// }
+				}
+			}
 		}
 			
 		
@@ -101,7 +116,7 @@ class Node implements Runnable{
 		}else{
 
 			neighbors[q][2] = Status.BRANCH.ordinal();   
-			state = NodeState.FOUND;    //we set the state of node to be found           
+			state = NodeState.FOUND;    //set the state of node to be found           
 
 			level = 0;	  //set the level to 0 as it is only node in fragment
 			rec = 0;			
@@ -143,29 +158,51 @@ class Node implements Runnable{
 	public int processConnectMsg(Message msg){
 		// proceesing connect message received from other nearest node.
 		// Read Connect Message from your msg object
-		int qID = neighborsIndex.get(msg.sender);
+
+		int qID = neighborsIndex.get(msg.sender);  //here qID is actually index of neighbor array 
+
 		if(level < ((ConnectMessage)msg).level){
 			System.out.println(nodeId + "Processing Connect in rule LT");
 			neighbors[qID][2] = Status.BRANCH.ordinal();
-			neighbors[qID][3] = Status.BRANCH.ordinal();
+			neighbors[qID][3] = 0; //making qId child
 			sendInitiateMessage(msg.sender, this.level, this.fragmentId, this.state);
 			return 1;
 
 		}else if(neighbors[qID][2] == Status.BASIC.ordinal()){
-			//how to implement wait? // Can we call initialize or find... we need to invoke a method to
-			// continue execution. I think we can make a manager method.
+			
 			System.out.println(nodeId + "waitManager call...");
 			waitManager(msg);  // need to push back message else it will be lost... //EDIT 1: QUEUE? Edit 2: may be..
 
 		}else{
-			System.out.println(nodeId + "Processing Connect in rule LT");
+			System.out.println(nodeId + "Processing Connect in rule ET");
+			neighbors[qID][3] = 0;
+			sendInitiateMessage(msg.sender, this.level + 1, neighbors[qID][1], NodeState.FIND);
+			return 1;
 		}
 		return 0;
 
 	}
 	
+	private void processInitiateMsg(Message msg){
+		System.out.println("In processInitiateMsg method");
+
+		this.level = ((InitiateMessage)msg).level;
+		this.fragmentId = ((InitiateMessage)msg).fragmentName;
+		this.state = ((InitiateMessage)msg).state;
+		this.parent = msg.sender;
+
+		bestNode = -1;
+		bestWt = Integer.MAX_VALUE;  // Need to check the limit of weight...
+		testNode = -1;
+
+
+
+	}
 	private void waitManager(Message msg){
 		System.out.println("In waitManager");
+		flag = 1;
+		waitingCount++;
+		waitingMessage.add(msg);
 
 	}
 
@@ -189,7 +226,7 @@ class Node implements Runnable{
 	}
 
 	private Message getMessage(){
-		//List<Message> recvQ = Main.allMessageList.get(nodeId);
+		
 		Message msg;
 		synchronized(messageList){
 			//try{
@@ -211,17 +248,15 @@ class Node implements Runnable{
 	}
 
 	private void prepareNeighborList(List<Integer> listOfNeighbors){
-		//Need to fill neighbors[][] array
-		// I am thinking to make it numberOfneighbors*4 size array (neighborId, weight, statusOfEdge, parent/child/none)
-
+	
 		int i = 0;
 		System.out.println("Node: " + this.nodeId);
 		for(Integer q : listOfNeighbors){
 			//System.out.println(q + " " + Main.G[q][this.nodeId].getWeight());
-			neighbors[i][0] = q;
-			neighbors[i][1] = Main.G[q][this.nodeId].getWeight();
-			neighbors[i][2] = Status.BASIC.ordinal();
-			neighbors[i][3] = -1;
+			neighbors[i][0] = q;									//NodeId of neighbor connected to this edge
+			neighbors[i][1] = Main.G[q][this.nodeId].getWeight();	//Weight of edge
+			neighbors[i][2] = Status.BASIC.ordinal();				//Status of Edge
+			neighbors[i][3] = -1;									// child or parent 0: child and parent: 1
 			neighbors[i][4] = 0;  //For counting report messase  0: not received, 1: received need to reset when required
 			neighborsIndex.put(q, i);
 			i++;
